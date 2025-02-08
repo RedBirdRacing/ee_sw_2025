0de23f9f03e5175884a41ccd0c7469a68186ca65
CYL-Ariel <Cyla.hk@gmail.com>
2025-02-08 18:12:18 +0800

    Throttle deadzone

--- vcu_firmware/VCU_Firmware.ino
+++ vcu_firmware/VCU_Firmware.ino
@@ -1,22 +1,47 @@
+// #include <CAN.h>
+
 #include "pinMap.h"
-#include <can.h>
+// #include <can.h>
 #include <mcp2515.h>
 
 uint8_t pin_in[8] = {ADC1, ADC2};
 uint8_t pin_out[8] = {BTN1, BTN2, BTN3, BTN4, LED1, LED2, LED3};
 
-can_frame tx_msg;
-can_frame rx_msg;
+MCP2515 mcp2515(PC0); // CS pin PC0
+
+struct can_frame tx_msg;
+struct can_frame rx_msg;
+
+float MAX_THROTTLE_VAL = 4.3;
+float MIN_THROTTLE_VAL = 0.7;
+float THROTTLE_LOWER_DEADZONE_MAX = 0.5;
+float THORTTLE_UPPER_DEADZONE_MIN = 4.5;
+/*
+Between 0V and THROTTLE_LOWER_DEADZONE_MIN: Error for open circuit
+Between THROTTLE_LOWER_DEADZONE_MIN and MIN_THROTTLE_VAL: 0% Torque
+Between MIN_THROTTLE_VAL and MAX_THROTTLE_VAL: Linear relationship 
+Between MAX_THROTTLE_VAL and THORTTLE_UPPER_DEADZONE_MIN: 100% Torque
+Between THORTTLE_UPPER_DEADZONE_MIN and 5V: Error for short circuit
+*/
 
 uint16_t get_throttle_torque_val(float throttle_val)
 {
-    uint16_t throttle_val = analogRead(ADC1);
+    float throttle_volt = analogRead(ADC1) / 1024 * 5; // Outputs a value between 0 and 1023 inclusive
 
-    // Currently just map a linear relationship, with 5V as 100% torque and 0V as 0% torque
+    uint16_t ret = 0;
 
-    // Scale up the value for canbus
-    // For torque, maximum canbus value is 32760, which is 32 times the maximum value of analogRead (1023)
-    throttle_val *= 20;
+    if (throttle_volt < THROTTLE_LOWER_DEADZONE_MAX)
+        ret = 0;
+    else if (throttle_volt < MIN_THROTTLE_VAL) 
+        ret = 0;
+    else if (throttle_volt < MAX_THROTTLE_VAL)
+        // Scale up the value for canbus
+        // For torque, maximum canbus value is 32760
+        ret = (throttle_volt - MIN_THROTTLE_VAL) / (MAX_THROTTLE_VAL - MIN_THROTTLE_VAL) * 32760;
+    else if (throttle_volt < THORTTLE_UPPER_DEADZONE_MIN)
+        ret = 32760;
+    else
+        ret = 0;
 
     return throttle_val;
 }
@@ -48,10 +73,10 @@ void setup()
     for (int i = 0; i < 7; i++)
         pinMode(pin_out[i], OUTPUT);
 
-    MCP2515 mcp2515(PC0); // CS pin PC0
+    // MCP2515 mcp2515(PC0); // CS pin PC0
     mcp2515.reset();
-    mcp2515.setBierate(CAN_500KBPS, MCP_20MHZ);
-    mcp.setNormalMode();
+    mcp2515.setBitrate(CAN_500KBPS, MCP_16MHZ);
+    mcp2515.setNormalMode();
 }
 
 void loop()
@@ -66,7 +91,7 @@ void loop()
                 digitalWrite(pin_out[i], (rx_msg.data[0] >> i) & 0x01);
     }
 
-    mcp2515.sendMessage(tx_msg);
+    mcp2515.sendMessage(&tx_msg);
 }
 
 // #include "Pedal.h"
