11ffc30715b0f8bfa0ace23fe0321090392868eb
CYL-Ariel <Cyla.hk@gmail.com>
2025-03-06 22:00:37 +0800

    Init folder
    The .txt file is just a dummy file for having the folder on git

--- vcu_firmware/VCU_Firmware.ino
+++ vcu_firmware/VCU_Firmware.ino
@@ -1,13 +1,13 @@
-// #include <CAN.h>
-
 #include "pinMap.h"
-// #include <can.h>
+#include <can.h>
+// #include <SPI.h>
 #include <mcp2515.h>
 
-uint8_t pin_in[8] = {ADC1, ADC2};
-uint8_t pin_out[8] = {BTN1, BTN2, BTN3, BTN4, LED1, LED2, LED3};
+// uint8_t pin_in[8] = {ADC1, ADC2};
+// uint8_t pin_out[8] = {BTN1, BTN2, BTN3, BTN4, LED1, LED2, LED3};
 
-MCP2515 mcp2515(PC0); // CS pin PC0
+// MCP2515 mcp2515(PB2); // CS pin PC0; PB2 for testing on arduino uno
+MCP2515 mcp2515(10);
 
 struct can_frame tx_throttle_msg;
 struct can_frame rx_msg;
@@ -22,6 +22,7 @@ const int MIN_THROTTLE_OUT_VAL = 300; // Minium torque value tested is 300 (TBC)
 
 uint16_t get_throttle_torque_val()
 {
+    return MIN_THROTTLE_OUT_VAL;
     float throttle_volt = analogRead(ADC1) / 1024 * 5; // Converts analogRead output to a float between 0V and 5V
 
     uint16_t ret = 0;
@@ -53,8 +54,8 @@ uint16_t update_can_tx_throttle_msg()
     // Set the can_frame according to the torque map
     uint16_t throttle_torque_val = get_throttle_torque_val();
 
-    if (throttle_torque_val == 0) // Error in reading torque value
-      return 1;
+    // if (throttle_torque_val == 0) // Error in reading torque value
+    //   return 1;
 
     tx_throttle_msg.can_id = 201;
     tx_throttle_msg.can_dlc = 3;
@@ -68,31 +69,45 @@ uint16_t update_can_tx_throttle_msg()
 void setup()
 {
     // Init pins
-    for (int i = 0; i <  2; i++)
-        pinMode(pin_in[i], INPUT);
-    for (int i = 0; i < 7; i++)
-        pinMode(pin_out[i], OUTPUT);
+    // for (int i = 0; i <  2; i++)
+    //     pinMode(pin_in[i], INPUT);
+    // for (int i = 0; i < 7; i++)
+    //     pinMode(pin_out[i], OUTPUT);
 
     // Init mcp2515 
     mcp2515.reset();
-    mcp2515.setBitrate(CAN_500KBPS, MCP_16MHZ);
+    mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ); // 8MHZ for testing on uno
     mcp2515.setNormalMode();
+    // while (!Serial);
+    //   Serial.begin(9600);
 }
 
 void loop()
 {
     // Send throttle torque msg if no error
-    if (update_can_tx_throttle_msg() == 0)
-        mcp2515.sendMessage(&tx_throttle_msg);
+    // if (update_can_tx_throttle_msg() == 0)
+        // mcp2515.sendMessage(&tx_throttle_msg);
+    tx_throttle_msg.can_id = 201;
+    tx_throttle_msg.can_dlc = 3;
+    tx_throttle_msg.data[0] = 0x90; //0x90 for torque, 0x31 for speed
+    tx_throttle_msg.data[1] = 0x28;
+    tx_throttle_msg.data[2] = 0x19;
+    mcp2515.sendMessage(&tx_throttle_msg);
+    
+    // Serial.println("Example: Write to CAN");
+    // Serial.println(temp.data[0]);
 
-    uint32_t lastLEDtick = 0;
-    if (mcp2515.readMessage(&rx_msg) == MCP2515::ERROR_OK)
-    {
-        // Commented out as currenlty no need to include receive functionality
-        // if (rx_msg.can_id == 0x522)
-        //     for (int i = 0; i < 8; i++)
-        //         digitalWrite(pin_out[i], (rx_msg.data[0] >> i) & 0x01);
-    }
+    delay(100);
+
+    // mcp2515.sendMessage(&tx_throttle_msg);
+    // uint32_t lastLEDtick = 0;
+    // if (mcp2515.readMessage(&rx_msg) == MCP2515::ERROR_OK)
+    // {
+    //     // Commented out as currenlty no need to include receive functionality
+    //     // if (rx_msg.can_id == 0x522)
+    //     //     for (int i = 0; i < 8; i++)
+    //     //         digitalWrite(pin_out[i], (rx_msg.data[0] >> i) & 0x01);
+    // }
 }
 
 // #include "Pedal.h"
@@ -109,4 +124,55 @@ void loop()
 
 // void loop() {
 //   throttle.pedalUpdate(millis());
+// }
+// #include <mcp_canbus.h>
+// // #include <mcp2515.h>
+// #include <SPI.h>
+
+// // uint8_t pinout[8] = {PA0, PA1, PA3, PA7, PB0, PB1, PB2, PB3};
+
+
+// #define CAN_500KBPS
+// const int SPI_CS_PIN = PB2;
+// // MCP_CAN mcp2515(SPI_CS_PIN); 
+// MCP2515 mcp2515(SPI_CS_PIN);
+
+// void setup(){
+    
+// //     // pinMode(PB6, INPUT);
+
+//     // for(int i = 0; i < 8; i++){
+//     //     pinMode(pinout[i], OUTPUT);
+//     // }
+
+//     mcp2515.reset();
+//     mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
+//     mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
+//     mcp2515.setNormalMode();
+
+//     struct can_frame tx_msg;
+//     tx_msg.can_id = 0x520;
+//     tx_msg.can_dlc = 3;
+//     tx_msg.data[0] = 0x11;
+//     tx_msg.data[1] = 0x22;
+//     tx_msg.data[2] = 0x33;
+//     mcp2515.sendMessage(&tx_msg);
+
+//     struct can_frame rx_msg;
+
+//     uint32_t lastLEDtick = 0;
+
+//     while (1){
+        
+//         if(mcp2515.readMessage(&rx_msg) == MCP2515::ERROR_OK){
+//             if(rx_msg.can_id == 0x522){
+
+//                 // for(int i = 0; i < 8; i++){
+//                 //     digitalWrite(pinout[i], (rx_msg.data[0] >> i) & 0x01);
+//                 // }
+
+//                 mcp2515.sendMessage(&tx_msg);
+//             }
+//         }
+//     }
 // }
